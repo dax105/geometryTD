e24f72bcb3af8e5c7041afda7a574e318b0bc9be
dax105 <jerry1921mail@gmail.com>
2014-12-07 14:33:13 +0100

    ptt

--- src/cz/dat/geometrytd/Game.java
+++ src/cz/dat/geometrytd/Game.java
@@ -11,39 +11,42 @@ public class Game implements Runnable {
 	public static final int WINDOW_WIDTH = 854;
 	public static final int WINDOW_HEIGHT = 480;
 	public static final String GAME_NAME = "Geometry TD";
-	
+
 	private static final int TPS = 20;
 	private static final double TICK_TIME = 1.0D / TPS;
-	
+
 	private void tick(int ticks) {
-		
+
 	}
-	
+
 	private void renderTick(float ptt) {
-		
+
 	}
-	
+
 	@Override
 	public void run() {
-		initDisplay();
-		
+		Game.initDisplay();
+
 		long time = System.nanoTime();
 		long lastTime = time;
 		long lastInfo = time;
-		
+
 		int fps = 0;
 		int ticks = 0;
 		int lastTicks = 0;
-		
+
 		while (!Display.isCloseRequested()) {
 			int e = GL11.glGetError();
-			if(e != 0) {
-				System.err.println("GL ERROR: " + e + " - " + GLU.gluErrorString(e));
-			}	
-			
-			this.renderTick(1);
+			if (e != 0) {
+				System.err.println("GL ERROR: " + e + " - "
+						+ GLU.gluErrorString(e));
+			}
+
+			float ptt = (time - lastTime) / ((float) Game.TICK_TIME * 1000000000);
+
+			this.renderTick(ptt);
 			fps++;
-			
+
 			time = System.nanoTime();
 			while (time - lastTime >= Game.TICK_TIME * 1000000000) {
 				this.tick(ticks++);
@@ -52,20 +55,21 @@ public class Game implements Runnable {
 
 			if (time - lastInfo >= 1000000000) {
 				lastInfo += 1000000000;
-				Display.setTitle(GAME_NAME + " - " + fps + " fps - " + (ticks - lastTicks) + " tps");
+				Display.setTitle(Game.GAME_NAME + " - " + fps + " fps - "
+						+ (ticks - lastTicks) + " tps");
 				lastTicks = ticks;
 				fps = 0;
 			}
-			
+
 			Display.update();
 		}
 	}
-	
+
 	public static void initDisplay() {
 		try {
-			DisplayMode d = new DisplayMode(WINDOW_WIDTH, WINDOW_HEIGHT);
+			DisplayMode d = new DisplayMode(Game.WINDOW_WIDTH, Game.WINDOW_HEIGHT);
 			Display.setDisplayMode(d);
-			Display.setTitle(GAME_NAME);
+			Display.setTitle(Game.GAME_NAME);
 			Display.create();
 		} catch (LWJGLException e) {
 			e.printStackTrace();
@@ -77,5 +81,5 @@ public class Game implements Runnable {
 		Thread t = new Thread(new Game());
 		t.start();
 	}
-	
+
 }
