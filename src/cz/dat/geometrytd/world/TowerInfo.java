3501f541daf1ea93a8174bcabb273c94f69fb3f4
LeOndryasO <ondryaso@ondryaso.eu>
2014-12-08 22:56:02 +0100

    Working tower info

--- src/cz/dat/geometrytd/world/TowerInfo.java
+++ src/cz/dat/geometrytd/world/TowerInfo.java
@@ -1,5 +1,7 @@
 package cz.dat.geometrytd.world;
 
+import org.newdawn.slick.Color;
+
 import cz.dat.geometrytd.TickListener;
 import cz.dat.geometrytd.gl.GLUtil;
 
@@ -8,18 +10,25 @@ public class TowerInfo extends TickListener {
 	private World w;
 	private Tower currentTower;
 	private boolean show;
-	
-	int startX;
-	
-	public TowerInfo(World world) {
+
+	private int startX, levelY, rangeY, damageY;
+
+	public TowerInfo(World world, int startX, int startY) {
 		super(world.getGame());
 		this.w = world;
+
+		this.startX = startX;
+		this.levelY = startY + 32;
+		this.rangeY = this.levelY
+				+ this.game.getFontManager().getFont().getHeight() - 12;
+		this.damageY = this.rangeY
+				+ this.game.getFontManager().getFont().getHeight() - 12;
 	}
-	
+
 	public void setShowed(boolean state) {
 		this.show = state;
 	}
-	
+
 	public void openTower(Tower t) {
 		this.currentTower = t;
 		this.setShowed(true);
@@ -32,10 +41,29 @@ public class TowerInfo extends TickListener {
 
 	@Override
 	protected void renderTick(float ptt) {
-		if(this.show) {
-			GLUtil.drawCircle(this.currentTower.rec.x + (this.currentTower.rec.width / 2),
-					this.currentTower.rec.y + (this.currentTower.rec.height / 2), this.currentTower.range);
+		if (this.show) {
+			GLUtil.drawCircle(this.currentTower.rec.x
+					+ (this.currentTower.rec.width / 2),
+					this.currentTower.rec.y
+							+ (this.currentTower.rec.height / 2),
+					this.currentTower.range);
+
+			this.game.getFontManager().drawString(
+					"Level: "
+							+ this.currentTower.getLevel()
+							+ " (next: "
+							+ this.currentTower.getLevelCost(this.currentTower
+									.getLevel() + 1) + " points)", this.startX,
+					this.levelY, Color.white);
+			this.game.getFontManager().drawString(
+					"Range: " + this.currentTower.getRange() + " (+"
+							+ this.currentTower.getNextRange() + ")",
+					this.startX, this.rangeY, Color.white);
+			this.game.getFontManager().drawString(
+					"Damage: " + this.currentTower.getDamage() + " (+"
+							+ this.currentTower.getNextDamage() + ")",
+					this.startX, this.damageY, Color.white);
+			this.game.getTextureManager().clearBind();
 		}
 	}
-
 }
